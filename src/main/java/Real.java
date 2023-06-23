beea866d02116e135e5bb3ecd5c25bdba7ff860d
Bohdan Senyk <senyk2001@gmail.com>
2023-06-23 12:34:48 +0300

    refactored code according to checkstyle file

--- src/main/java/Real.java
+++ src/main/java/Real.java
@@ -1,30 +1,30 @@
 public class Real extends Product {
-   private int size;
-   private int weight;
+    private int size;
+    private int weight;
 
-   public int getSize() {
-       return size;
-   }
+    public int getSize() {
+        return size;
+    }
 
-   public void setSize(int size) {
-       this.size = size;
-   }
+    public void setSize(int size) {
+        this.size = size;
+    }
 
-   public int getWeight() {
-       return weight;
-   }
+    public int getWeight() {
+        return weight;
+    }
 
-   public void setWeight(int weight) {
-       this.weight = weight;
-   }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 
-   @Override
-   public String toString() {
-       return "Real{" +
-               "name='" + getName() +
-               ", price=" + getPrice() +
-               ", size=" + size +
-               ", weight=" + weight +
-               '}';
-   }
+    @Override
+    public String toString() {
+        return "Real{"
+                + "name='" + getName()
+                + ", price=" + getPrice()
+                + ", size=" + size
+                + ", weight=" + weight
+                + '}';
+    }
 }
