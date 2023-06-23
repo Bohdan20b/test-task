f4e6402fd79eea215629eec528bc555f27ef464a
Bohdan Senyk <senyk2001@gmail.com>
2023-06-23 12:05:52 +0300

    add implemented task

---
+++ src/main/java/User.java
@@ -0,0 +1,38 @@
+public class User {
+   private String name;
+   private int age;
+
+   private User() {
+   }
+
+   public static User createUser(String name, int age) {
+       User user = new User();
+       user.name = name;
+       user.age = age;
+       return user;
+   }
+
+   public String getName() {
+       return name;
+   }
+
+   public void setName(String name) {
+       this.name = name;
+   }
+
+   public int getAge() {
+       return age;
+   }
+
+   public void setAge(int age) {
+       this.age = age;
+   }
+
+   @Override
+   public String toString() {
+       return "User{" +
+               "name='" + name + '\'' +
+               ", age=" + age +
+               '}';
+   }
+}
